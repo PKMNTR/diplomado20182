152f9fdf9aaf7cd10556e84182804458aec1fe78
d182_Ricardo_R <d182_Ricardo_R@macbookpro-20.local>
2018-02-26 13:00:18 -0600

    Add WorldTrotter

---
+++ Tarea/Proyectos/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by d182_Ricardo_R on 26/02/18.
+//  Copyright © 2018 d182_Ricardo_R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+//    Codigo de practica de Views y su jerarquia
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        let firstFrame = CGRect(x:160, y:240, width: 100, height: 150)
+//        let firstView = UIView(frame: firstFrame)
+//
+//        firstView.backgroundColor = UIColor.blue
+//        view.addSubview(firstView)
+//
+//        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+//        let secondView = UIView(frame: secondFrame)
+//
+//        secondView.backgroundColor = UIColor.gray
+//        firstView.addSubview(secondView)
+//    }
+    
+}
+
