223f5f11141116289130fc06b78c5d406183e17a
user140663 <user140663@TX005.local>
2018-05-28 22:27:03 -0700

    agrego archivos capitulo 7

---
+++ Tarea/Proyectos/Capitulo7/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by d182_Ricardo_R on 16/03/18.
+//  Copyright © 2018 d182_Ricardo_R. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate{
+    @IBOutlet var celsiusLabel : UILabel!
+    @IBOutlet var textField : UITextField!
+    var fahrenheitValue : Measurement<UnitTemperature>?
+    {
+        didSet{
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue : Measurement<UnitTemperature>?
+    {
+        if let fahrenheitValue = fahrenheitValue
+        {
+            return fahrenheitValue.converted(to: .celsius)
+        }
+        else
+        {
+            return nil
+        }
+    }
+    
+    let numberFormatter : NumberFormatter = {
+        let nf  = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.maximumFractionDigits = 0
+        nf.minimumFractionDigits = 1
+        return nf
+    }()
+    
+    func updateCelsiusLabel()
+    {
+        if let celsiusValue = celsiusValue
+        {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        }
+        else{
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField)
+    {
+        if let text = textField.text, let value = Double(text)
+        {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        }
+        else{
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer)
+    {
+        textField.resignFirstResponder()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ConversionViewController cargado")
+        
+        updateCelsiusLabel()
+    }
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        if existingTextHasDecimalSeparator != nil,
+            replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true }
+    }
+    
+}
+
+
---
+++ Tarea/Proyectos/Capitulo7/WorldTrotter/MapViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by user140663 on 5/27/18.
+//  Copyright © 2018 d182_Ricardo_R. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController : UIViewController{
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        
+        view = mapView
+        
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satelite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(MapViewController.mapTypeChanged(_:)), for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let margins = view.layoutMarginsGuide
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingCosntraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingCosntraint.isActive = true
+        
+    }
+    
+    override func viewDidLoad() {
+        print("MapViewController cargado")
+    }
+    
+    @objc func mapTypeChanged(_ segControl:UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+}
